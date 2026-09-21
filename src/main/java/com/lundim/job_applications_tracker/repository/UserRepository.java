07206895e110968591c0181441a88b324f4271c5
Lundi <keabetswelundim@gmail.com>
2026-09-21 15:26:26 +0200

    refactor: add @Repository annotation

--- src/main/java/com/lundim/job_applications_tracker/repository/UserRepository.java
+++ src/main/java/com/lundim/job_applications_tracker/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.lundim.job_applications_tracker.repository;
 
 import com.lundim.job_applications_tracker.model.entity.CustomUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<CustomUser, Long> {
 
     Optional<CustomUser> findByEmail(String email);
